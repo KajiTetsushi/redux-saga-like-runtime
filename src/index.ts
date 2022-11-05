fde5d3033351b69bea3d5b9e05b69845493e22aa
Kaji Tetsushi <kajitetsushi@outlook.com>
2022-11-05 22:21:29 +0800

    build(app): scaffold with parcel and typescript

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata';
+
+const heading = 'redux-saga-like-runtime';
+const headingElement = document.createElement('h1');
+headingElement.innerText = heading;
+document.body.appendChild(headingElement);
+
+const paragraph = 'Or... I did not know I could write Sagas without Redux!';
+const paragraphElement = document.createElement('p');
+paragraphElement.innerText = paragraph;
+document.body.appendChild(paragraphElement);
